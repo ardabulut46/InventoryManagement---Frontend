dd69c804f7c1eaa943cff6123620b87529409cbf
Arda Bulut <arda.bulut-08@outlook.com>
2025-02-12 20:21:18 +0300

    backend url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 //export const API_BASE_URL ='http://localhost:5192';
 //export const API_URL = API_BASE_URL;
 // API URL configuration
-export const API_BASE_URL = 'http://localhost:5192';
+export const API_BASE_URL = 'https://hysistemi-ehdghjdubmb7gudg.northeurope-01.azurewebsites.net';
 export const API_URL = API_BASE_URL;
 // Add other configuration constants here if needed 
-//import.meta.env.VITE_API_URL || 
+//import.meta.env.VITE_API_URL || s
