dc86bb983d9ecb2cebd3a90e5e6272f63a59af46
Arda Bulut <arda.bulut-08@outlook.com>
2025-04-02 14:32:42 +0300

    config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 // API URL configuration
-//export const API_BASE_URL ='https://hysistemi-ehdghjdubmb7gudg.northeurope-01.azurewebsites.net';
-//export const API_URL = API_BASE_URL;
-// API URL configuration
-export const API_BASE_URL = 'https://hysistemi-ehdghjdubmb7gudg.northeurope-01.azurewebsites.net';
+const isProd = window.location.hostname !== 'localhost';
+
+export const API_BASE_URL = isProd 
+  ? 'https://hysistemi-ehdghjdubmb7gudg.northeurope-01.azurewebsites.net'
+  : 'http://localhost:5192';
+  
 export const API_URL = API_BASE_URL;
 // Add other configuration constants here if needed 
 //import.meta.env.VITE_API_URL || 
